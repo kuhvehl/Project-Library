35ab73ef7a2b43a2c724030e5ede6c1f14b8bce0
kuhvehl <96754228+kuhvehl@users.noreply.github.com>
2024-07-25 10:56:59 -0400

    Add a “NEW BOOK” button that brings up a form
    Add a “NEW BOOK” button that brings up a form
    allowing users to input the details for the new
    book: author, title, number of pages, whether it’s
    been read

--- script.js
+++ script.js
@@ -17,38 +17,82 @@ addBookToLibrary('test book 3', 'test author 3', 300, false);
 addBookToLibrary('test book 4', 'test author 4', 400, false);
 
 const bookCards = document.querySelector('.book-cards')
+const addBook = document.querySelector(".add-book");
+const dialog = document.querySelector('dialog');
+const cancelButton = document.querySelector('.cancel');
+const submitButton = document.querySelector('.submit');
 
-myLibrary.forEach(function addBook(book) {
-    const bookCard = document.createElement('div');
-    const title = document.createElement('div');
-    const author = document.createElement('div')
-    const pages = document.createElement('div')
-    const read = document.createElement('button')
-    const remove = document.createElement('button')
-    const readText = readMessage(book.read);
+const titleInput = document.querySelector('#title')
+const authorInput = document.querySelector('#author')
+const pagesInput = document.querySelector('#pages')
+const readInput = document.getElementsByName('readStatus')
+const form = document.querySelector("form");
 
-    bookCard.classList.add('book-card');
-    title.classList.add('title');
-    author.classList.add('author');
-    pages.classList.add('pages');
-    read.classList.add('toggle-read');
-    remove.classList.add('remove');
+addBook.addEventListener("click", () => {
+    titleInput.value = ''
+    authorInput.value = ''
+    pagesInput.value = ''
+    readInput[0].checked = true;
+    readInput[1].checked = false;
+    
+    dialog.showModal();
+  });
 
-    title.textContent = book.title;
-    author.textContent = book.author;
-    pages.textContent = `${book.pages} pages`;
-    read.textContent = readText;
-    remove.textContent = 'Remove';
+dialog.addEventListener('close', (e) => {
+    if (dialog.returnValue === 'submit') {
+        const newTitle = form.title.value;
+        const newAuthor = form.author.value;
+        const newPages = form.pages.value;
+        const newStatus = form.readStatus.value;
 
-    bookCard.appendChild(title);
-    bookCard.appendChild(author);
-    bookCard.appendChild(pages);
-    bookCard.appendChild(read);
-    bookCard.appendChild(remove);
-
-    bookCards.append(bookCard);
+        addBookToLibrary(newTitle, newAuthor, newPages, newStatus)
+        clearBooks()
+        displayBooks();
+    }
 })
+
+function displayBooks() {
+    myLibrary.forEach(function addBook(book) {
+        const bookCard = document.createElement('div');
+        const title = document.createElement('div');
+        const author = document.createElement('div')
+        const pages = document.createElement('div')
+        const read = document.createElement('button')
+        const remove = document.createElement('button')
+        const readText = readMessage(book.read);
+        
+        bookCard.classList.add('book-card');
+        title.classList.add('title');
+        author.classList.add('author');
+        pages.classList.add('pages');
+        read.classList.add('toggle-read');
+        remove.classList.add('remove');
+        
+        title.textContent = book.title;
+        author.textContent = book.author;
+        pages.textContent = `${book.pages} pages`;
+        read.textContent = readText;
+        remove.textContent = 'Remove';
+        
+        bookCard.appendChild(title);
+        bookCard.appendChild(author);
+        bookCard.appendChild(pages);
+        bookCard.appendChild(read);
+        bookCard.appendChild(remove);
+        bookCards.append(bookCard);
+    })
+}
+
+function clearBooks() {
+    let sibling = dialog.nextSibling
+    while (sibling) {
+        dialog.nextSibling.remove();
+        sibling = dialog.nextSibling
+    }
+}
 
 function readMessage(status) {
     return status ? 'Read' : 'Not read';
 }
+
+displayBooks();
